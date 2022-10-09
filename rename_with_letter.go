844de598f25c7e16a62fd38698ca91ca18e4f511
BoppreH <github@boppreh.com>
2022-10-09 14:40:33 +0200

    Add restore_filenames program

--- rename_with_letter.go
+++ rename_with_letter.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strings"
-    "path/filepath"
-    "sort"
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
 )
 
 func main() {
@@ -25,52 +25,52 @@ func main() {
 	// Path of the folder with the files to rename.
 	path := os.Args[1]
 
-    // Find all files in the given folder. Lazy solution by matching all names with a dot in it.
-    files, err := filepath.Glob(filepath.Join(path, "*.*"))
-    if err != nil {
-    	fmt.Printf("ERROR: Could not read files in %s because of: %s\n", path, err)
-    	fmt.Printf("Press any key to exit...")
-    	fmt.Scanln()
-    	os.Exit(2)
-    }
-    fmt.Printf("Will rename the following files: %s\n\n", files)
+	// Find all files in the given folder. Lazy solution by matching all names with a dot in it.
+	files, err := filepath.Glob(filepath.Join(path, "*.*"))
+	if err != nil {
+		fmt.Printf("ERROR: Could not read files in %s because of: %s\n", path, err)
+		fmt.Printf("Press any key to exit...")
+		fmt.Scanln()
+		os.Exit(2)
+	}
+	fmt.Printf("Will rename the following files: %s\n\n", files)
 
 	// Characters that must not appear in a Windows filename.
 	BAD_CHARACTERS := ".\\/:<>\"|?*"
 
 	// Ask for the characters to add, in a command prompt window.
 	// If there's a problem, keep trying until a valid answer is given.
-    var str string
-    reader := bufio.NewReader(os.Stdin)
-    for {
-        fmt.Printf("What letter to add to the files in %s? ", path)
-        str, _ = reader.ReadString('\n')
-        if str == "" {
-        	fmt.Printf("ERROR: Cannot be empty.\n\n")
-        } else if strings.ContainsAny(str, BAD_CHARACTERS) {
-        	fmt.Printf("ERROR: Windows filenames cannot contain any of the following characters: %s\n\n", BAD_CHARACTERS)
-        } else {
-        	break
-        }
-    }
-    prefix := strings.TrimSpace(str)
+	var str string
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Printf("What letter to add to the files in %s? ", path)
+		str, _ = reader.ReadString('\n')
+		if str == "" {
+			fmt.Printf("ERROR: Cannot be empty.\n\n")
+		} else if strings.ContainsAny(str, BAD_CHARACTERS) {
+			fmt.Printf("ERROR: Windows filenames cannot contain any of the following characters: %s\n\n", BAD_CHARACTERS)
+		} else {
+			break
+		}
+	}
+	prefix := strings.TrimSpace(str)
 
-    // Sort list of files by length, longest first. This guarantees that adding a prefix will not replace an existing file.
+	// Sort list of files by length, longest first. This guarantees that adding a prefix will not replace an existing file.
 	sort.Slice(files, func(i, j int) bool {
-        return len(files[i]) > len(files[j])
-    })
+		return len(files[i]) > len(files[j])
+	})
 
-    // Rename the files in the folder.
-    for _, file := range files {
-    	newFile := filepath.Join(filepath.Dir(file), prefix + filepath.Base(file))
-    	fmt.Printf("mv %s %s\n", file, newFile)
-    	err = os.Rename(file, newFile)
-    	if err != nil {
-    		fmt.Printf("ERROR: failed to rename %s to %s because of: %s. Continuing with the rest of the files...\n", file, newFile, err)
-    	}
-    }
+	// Rename the files in the folder.
+	for _, file := range files {
+		newFile := filepath.Join(filepath.Dir(file), prefix+filepath.Base(file))
+		fmt.Printf("mv %s %s\n", file, newFile)
+		err = os.Rename(file, newFile)
+		if err != nil {
+			fmt.Printf("ERROR: failed to rename %s to %s because of: %s. Continuing with the rest of the files...\n", file, newFile, err)
+		}
+	}
 
-    fmt.Println("\n\nAll done!\n")
+	fmt.Println("\n\nAll done!\n")
 	fmt.Println("Press any key to exit...")
 	fmt.Scanln()
 	os.Exit(0)
---
+++ restore_filenames.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+    "regexp"
+    "io/ioutil"
+)
+
+func die(status int) {
+	fmt.Println("Press any key to exit...")
+	fmt.Scanln()
+	os.Exit(status)
+}
+
+func regexGlobOrDie(path string, patternString string) (matches []string) {
+    entries, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Printf("ERROR: Could not read files in %s because of: %s\n", path, err)
+		die(2)
+	}
+
+    pattern := regexp.MustCompile("^" + patternString + "$")
+    for _, entry := range entries {
+        if !entry.IsDir() && pattern.MatchString(entry.Name()) {
+            matches = append(matches, filepath.Join(path, entry.Name()))
+        }
+    }
+
+	return matches
+}
+
+func main() {
+	fmt.Println("LT_Tom, welcome to Filename Restorer!")
+	fmt.Println("This program takes two folders, and renames the files in one to match the names in the other.")
+	fmt.Println()
+
+	if len(os.Args) != 3 {
+		fmt.Println("ERROR")
+		fmt.Println("Please run this program by dragging and dropping exactly two folders on it.")
+		die(1)
+	}
+
+	// Path of the folders with the files to rename.
+	path1 := os.Args[1]
+	path2 := os.Args[2]
+
+	// Find all files in the given folder. Lazy solution by matching all names with a dot in it.
+	numberedFiles1 := regexGlobOrDie(path1, `\d+\..+`)
+	numberedFiles2 := regexGlobOrDie(path2, `\d+\..+`)
+
+	if len(numberedFiles1) == 0 && len(numberedFiles2) == 0 {
+		fmt.Println("ERROR: Neither folder contains files with numeric names. Don't know which one to rename.")
+		die(3)
+	}
+
+	var sourceFiles, destFiles []string
+	if len(numberedFiles1) > len(numberedFiles2) {
+		destFiles = numberedFiles1
+		sourceFiles = regexGlobOrDie(path2, `.+\..+`)
+	} else {
+		destFiles = numberedFiles2
+		sourceFiles = regexGlobOrDie(path1, `.+\..+`)
+	}
+
+	if len(sourceFiles) != len(destFiles) {
+		fmt.Println("ERROR: the number of file in the folders doesn't match.")
+		die(3)
+	}
+
+	// Rename the files in the folder.
+	for i := 0; i < len(destFiles); i++ {
+		sourceFile := sourceFiles[i]
+		destFile := destFiles[i]
+
+		newDestFile := filepath.Join(filepath.Dir(destFile), filepath.Base(sourceFile))
+		fmt.Printf("mv %s %s\n", destFile, newDestFile)
+		err := os.Rename(destFile, newDestFile)
+		if err != nil {
+			fmt.Printf("ERROR: failed to rename %s to %s because of: %s. Continuing with the rest of the files...\n", destFile, newDestFile, err)
+		}
+	}
+
+	fmt.Println("\n\nAll done!\n")
+	die(0)
+}
